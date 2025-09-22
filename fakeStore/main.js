63c224d6f68173a6938f282453a604a62265c7db
Yaman <yamandhruw750@gmail.com>
2025-09-22 13:20:45 +0530

    fix some box

--- fakeStore/main.js
+++ fakeStore/main.js
@@ -1,36 +1,20 @@
+const cardContainer = document.querySelector(".cardContainer");
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) => {
-    const cardContainer = document.querySelector(".Container");
-
     data.map((data) => {
+      console.log(data);
       let card = document.createElement("div");
-      card.classList.add("card");
-
-      let cardImage = document.createElement("img");
-      cardImage.src = data.image;
-      cardImage.alt = data.title;
-      card.appendChild(cardImage)
-
-      let cardCategory = document.createElement("h3");
-      cardCategory.classList.add("category");
-      cardCategory.textContent = data.category;
-      card.appendChild(cardCategory)
-
-      let cardTitle = document.createElement("h2");
-      cardTitle.classList.add("title");
-      cardTitle.textContent = data.title;
-      card.appendChild(cardTitle)
-
-      let cardPrice = document.createElement("h3");
-      cardPrice.classList.add("Price");
-      cardPrice.textContent = `Price: ${data.price}`
-      card.appendChild(cardPrice)
-
-      let Buybtn = document.createElement('button')
-      Buybtn.classList.add('Buybtn');
-      Buybtn.textContent = "BUY NOW"
-      card.appendChild(Buybtn)
+      card.classList.add("productCard");
+      card.innerHTML = `<div class="imageContainer">
+          <img src="${data.image}" alt="${data.title}" id="productImage" />
+        </div>
+        <div class="productContent">
+        <p id="productCategory">${data.category}</p>
+          <h2 id="productTitle">${data.title}</h2>
+          <h3 id="productPrice">\$ ${data.price}</h3>
+          <button class="addToCartButton">Add to Cart</button>
+        </div>`;
 
       cardContainer.appendChild(card);
     });
